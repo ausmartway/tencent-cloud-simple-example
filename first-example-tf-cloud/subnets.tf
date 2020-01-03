911aa83ef33b289b6e8fefa4592a20c5ff6cc67f
Yulei Liu <yulei.liu@gmail.com>
2020-01-03 12:20:06 +1100

    Update subnets.tf

--- first-example-tf-cloud/subnets.tf
+++ first-example-tf-cloud/subnets.tf
@@ -1,7 +1,7 @@
 resource "tencentcloud_subnet" "web" {
   name              = "my test subnet"
   cidr_block        = "10.0.1.0/24"
-  availability_zone = "ap-guangzhou-3"
+  availability_zone = "${tc_avalibilityzone}"
   vpc_id            = "${tencentcloud_vpc.main.id}"
   route_table_id = "${tencentcloud_route_table.web.id}"
 }
@@ -9,7 +9,7 @@ resource "tencentcloud_subnet" "web" {
 resource "tencentcloud_subnet" "db" {
   name              = "my test subnet"
   cidr_block        = "10.0.2.0/24"
-  availability_zone = "ap-guangzhou-3"
+  availability_zone = "${tc_avalibilityzone}"
   vpc_id            = "${tencentcloud_vpc.main.id}"
   route_table_id = "${tencentcloud_route_table.db.id}"
 }
